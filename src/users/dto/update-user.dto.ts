3423e988fd3ef2b1d88615dad7c9fe8a4a2bc10e
oltthaqi <oltthaqi@gmail.com>
2025-12-11 23:33:18 +0100

    added email

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,5 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class UpdateUserDto {
   @ApiProperty({
@@ -23,12 +30,13 @@ export class UpdateUserDto {
   last_name: string;
 
   @ApiProperty({
-    description: 'The phone number',
+    description: 'The email',
     type: String,
   })
-  @IsOptional()
+  @IsNotEmpty()
   @IsString()
-  @MinLength(10)
-  @MaxLength(15)
-  phone_number: string;
+  @MinLength(5)
+  @MaxLength(100)
+  @IsEmail({}, { message: 'Invalid email format' })
+  email: string;
 }
